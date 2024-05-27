476ffe6009dd86a67fb230a6fd533572a613fa01
yhoungdev <yhoungdev@gmail.com>
2024-05-27 12:21:49 +0100

    chore: updated data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
-import Homepage from "./page/homepage";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Suspense } from "react";
+import Loader from "./components/micro/loader";
+
+const Home = React.lazy(() => import("./page/homepage"));
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={
+          <Suspense fallback={<Loader/>}>
+            <Home/>
+          </Suspense>
+        } />
       </Routes>
     </Router>
   );
--- src/page/homepage.tsx
+++ src/page/homepage.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-
 import Hero from "../components/hero";
 import ActivitySection from "../components/activitySection";
 import Footer from "../components/footer";
-
 import Tokenomics from "../components/sections/tokenomics";
 import RoadmapSection from "../components/sections/roadmap";
 import Presale from "../components/sections/presale";
 import Disclamer from "../components/sections/disclamer";
+
 
 const Homepage = () => {
   return (
